69408bca4e135cf060315d1347cd6a9024067f53
Krystal0712 <101920438+Krystal0712@users.noreply.github.com>
2022-12-02 18:50:40 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,132 +1,130 @@
-var screen = 0;
-var y=-20;
-var x=200;
-var spd = 2;
-var score= 0;
-var fish;
-var shark;
-var sea;
-var jaws;
-var pxlfont;
+''/****
+ * FSE100: examples for how to link multiple exercises together
+ *****/
 
-let homeButton;
-let canvasX = 600;
-let canvasY = 400;
+let currentActivity = 0;
+let menuButton, game1Button, game2Button, game3Button;
 
-function preload() {
-  soundFormats('mp3', 'ogg');
-	fish = loadImage('Fish.png');
-	shark = loadImage('Shark.png');
-	sea = loadImage('Sea.jpg');
-	jaws = loadSound('Jaws.mp3');
-	chomp = loadSound('Chomp.mp3');
-    end = loadImage('End.jpg');
-    pxlfont = loadFont('slkscrb.ttf');
+/***** 
+  * If you want to load images or sounds into your application,
+  * try using preload()
+  * https://p5js.org/reference/#/p5/preload
+  *****/
 
+function preload(){
+  // game1Preload();
+  // //game2Preload();
+  // game3Preload();
+
+  Ubuntu = loadFont("Ubuntu-Regular.ttf");
+  img = loadImage('2.jpg');
+}
+
+function switchToMM(){
+  currentActivity = 0;
+  background(img);
+  img.resize(600,400);
+  
+// Hide the home page button, show the activity buttons
+  menuButton.hide();
+  game1Button.show();
+  game2Button.show();
+  game3Button.show();
 
 }
 
 function setup() {
-  createCanvas(600, 400);
-	  jaws.play();
-  homeButton = createButton("Home");
-  homeButton.size(canvasX / 9, canvasY / 11);
-  homeButton.position(510, 20);
-  homeButton.mousePressed(goToHome);
-  homeButton.style('background-color', 'rgba(86,196,234,0.49)');
-  homeButton.style('color', 'white');
+  createCanvas(600,400); //1920*1080 size
+  noStroke();
+  textAlign(CENTER, CENTER);
+  background(img);
+  img.resize(600,400);
+  
+  menuButton = createButton('Back');
+  menuButton.position(20, 30);
+  menuButton.style('font-size', '18px');
+  menuButton.style('background-color', '#9E9E9E8C');
+  menuButton.hide();
+  
+  game1Button = createButton('Fishing');
+  game1Button.position(230,220);
+  game1Button.style("font-family", "Ubuntu-Regular");
+  game1Button.style('font-size', '35px');
+  game1Button.style('background-color', 'rgb(247,192,51)');
+  game1Button.style('color', 'rgb(39,100,29)');
+  game1Button.mousePressed(goToFish);
+  game1Button.show();
+  
+  game2Button = createButton('Piano');
+  game2Button.position(240,320);
+  game2Button.style("font-family", "Ubuntu-Regular");
+  game2Button.style('font-size', '35px');
+  game2Button.style('background-color', 'rgb(247,192,51)');
+  game2Button.style('color', 'rgb(39,100,29)');
+  game2Button.mousePressed(goToPiano);
+  game2Button.show();
+  
+  game3Button = createButton('Spaceship');
+  game3Button.position(210,130);
+  game3Button.style("font-family", "Ubuntu-Regular");
+  game3Button.style('font-size', '35px');
+  game3Button.style('background-color', 'rgb(247,192,51)');
+  game3Button.style('color', 'rgb(39,100,29)');
+  game3Button.mousePressed(goToSpace);
+  game3Button.show();
+  
 }
 
-function draw() {
-	if(screen == 0){
-    startScreen()
-  }else if(screen == 1){
-  	gameOn()
-  }else if(screen==2){
-  	endScreen()
-  }	
+function goToFish(){
+  window.open('https://editor.p5js.org/cattuyen03/full/O1SE4kaSe');
+  
 }
 
-function startScreen(){
-		 jaws.pause();
-		background(96, 157, 255)
-		image(sea,0,0,600, 400)
-		fill(255)
-		textAlign(CENTER);
-		textSize(20)
-        textFont(pxlfont)
-		text('WELCOME TO MY CATCHING GAME', width / 2, height / 2)
-        text('Move the shark to catch the target', width / 2, height / 2 + 30)
-		text('(click to start)', width / 2, height / 2 + 60);
-		resetName();
+function goToPiano(){ 
+  window.open('https://editor.p5js.org/cattuyen03/full/FXN4bXmNy');
+  
 }
 
-function gameOn(){
-	  imageMode(CENTER);
-	 // jaws.setVolume(0.1);
-	image(sea,width/2,height/2,600,500)
-			textSize(15)
-
-  text("SCORE = " + score, 70,20)
-  rectMode(CENTER)
-  // rect(mouseX,height-10,50,30)
-	image(shark,mouseX,height-50,70,100)
-	  // ellipse(x,y,20,20)
-		image(fish,x,y,60,50)
-
-
-	y+= spd;
-  if(y>height){
-  	screen =2
-	 jaws.stop();
-
-	 }
-  if(y>height-50 && x>mouseX-35 && x<mouseX+35){
-  	y=-20
-    spd+=0.5
-    score+= 1
-		chomp.play();
-
-  }
-	if(y==-20){
-  	pickRandom();
-  }
+function goToSpace(){
+  window.open('https://editor.p5js.org/cattuyen03/full/mhc_XeYk9');
+  
 }
 
-function pickRandom(){
-	x= random(20,width-20)
+// function draw() {  
+//   switch(currentActivity){
+//     case 0: 
+//       mainMenu();
+//       break;
+//     case 1: 
+//       game1Draw();
+//       break;
+//     //case 2: 
+//      // game2Draw();
+//     //  break;
+//     case 3: 
+//       game3Draw();
+//       break;
+//   }
+// }
+
+function mainMenu(){
+  background(img);
+ 
 }
 
-function endScreen(){
-        background(96, 157, 255)
-		image(end,300,200,600,500)
-		fill('rgb(34,104,159)')
-		// background(150)
-		//textAlign(RIGHT,500,200,400,300);
-         textSize(25);
-		text('GAME OVER', width / 2 - 100, height / 2 - 30)
-  	text("SCORE = " + score, width / 2 - 100, height / 2)
-         textSize(14);
-		text('(click to play again)', width / 2 - 100, height / 2 + 20);
-}
-
-function mousePressed(){
-	if(screen==0){
-  	screen=1
-		jaws.loop();
-	 jaws.play();
-
-  }else if(screen==2){
-  	screen=0
-  }
-}
-
-function resetName(){
-	  score=0;
-  	spd=2;
-  	y=-20;
-}
-
-function goToHome(){
-  window.open('https://editor.p5js.org/cattuyen03/full/AdWo6ZK2x');
-}
+/*****
+* mousePressed() is a reserved function that is called whenever
+* the user presses the mouse button in the application.
+*****/
+// function mousePressed(){
+//   // Only game 4 uses the mousePressed function, but the switch statement
+//   // makes it easy to add the mousePressed functionality for other games.
+//   switch(currentActivity){
+//     case 2: 
+//       game2MousePressed();
+//       break;
+//     case 4: 
+//       game4MousePressed();
+//       break;
+//   }
+// }
